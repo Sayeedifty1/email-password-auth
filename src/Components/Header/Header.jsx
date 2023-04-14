adaa8da8cadfabc556f35cee728e9676df141eb8
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-04-15 03:24:55 +0600

    login page added and nav bar classes added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css'
+import './Header.css';
+
 const Header = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/register-rbs">Register RBS</Link>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Email-Password-Auth</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/login" className="nav-link">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/register" className="nav-link">Register</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/register-rbs" className="nav-link">Register RBS</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Login = () => {
+
+    const handleLogIn= (event)=>{
+        event.prevent.Default();
+        const form = event.target;
+    }
     return (
-        <div>
-            <h1>This is log in</h1>
-        </div>
+        <Container>
+            <Row className="justify-content-center">
+                <Col md={6} sm={9} xs={12}>
+                    <h1 className='text-warning mb-4'>Please Login</h1>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" name="email" placeholder="Enter email" />
+                            
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="Login">
+                            Login
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
